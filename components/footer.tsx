7a979f4bbf1dece9f4aed75ef38ac29c7a110d90
Aneesh-382005 <aneesh382005@gmail.com>
2025-12-03 14:42:43 -0500

    Found an Issue? raise it here

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,11 +4,21 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">
-        &copy; 2025 Aneesh Grover. All rights reserved.
+        Made with ❤️ by Aneesh Grover.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> Runs on Next.js, TypeScript, Tailwind & Vercel - feedback is always welcome.
-
+      </p>
+      <p className="text-xs mt-2">
+        Found an Issue? Raise it here{" "}
+        <a
+          href="https://github.com/Aneesh-382005/portfolio/issues"
+          className="underline hover:text-blue-600"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          https://github.com/Aneesh-382005/portfolio/issues
+        </a>
       </p>
     </footer>
   );
