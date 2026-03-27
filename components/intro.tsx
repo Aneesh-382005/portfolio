c5c33e12421b837eae2ad2977be77a2b5d4d27d6
Aneesh-382005 <aneesh382005@gmail.com>
2026-03-26 20:08:25 -0400

    Open Resume in Browser instead of shoving a download

--- components/intro.tsx
+++ components/intro.tsx
@@ -89,7 +89,8 @@ export default function Intro() {
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/Aneesh_Grover_Resume.pdf"
-          download
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Get My Resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
